6c950f3fb01c88490271d6708f7aac30a84e0972
roddypr <rodrigopracana@gmail.com>
2018-10-16 17:45:51 +0100

    typos

--- simulate_2.r
+++ simulate_2.r
@@ -45,8 +45,7 @@ option_list <- list(
 opt_parser <- OptionParser(option_list=option_list)
 opt        <- parse_args(opt_parser)
 
-
-library(ggplot2)
+# ---------------------------------------------------------------------------- #
 
 simulateDeletions <- function(chromosome,
                               mutation_rate,
